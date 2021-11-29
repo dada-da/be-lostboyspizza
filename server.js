6a41c0d46c79050ba516150d896f5c90217e152f
dada-da <caoducanh.1501@gmail.com>
2021-11-30 01:43:58 +0700

    test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(
